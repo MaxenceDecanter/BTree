c4db2b485102f368679e4ffa6c7ae0fc21578671
MaxenceDecanter <maxence.decanter@hotmail.fr>
2018-04-01 14:16:53 +0200

    Initialisation

---
+++ ProjetAlgo/src/BTree.java
@@ -0,0 +1,134 @@
+import java.util.*;
+
+public class BTree {
+	
+	List<Integer> listKey = new ArrayList<Integer>();
+	List<BTree> listChildren = new ArrayList<BTree>();
+	int n =2;
+	BTree treeToSearchIn;
+	boolean res = false;
+	
+	public BTree(List<Integer> keys, List<BTree> tree){
+		this.listKey = keys;
+		this.listChildren = tree;
+	}
+	
+	public int getKey(int index){
+		return listKey.get(index);
+	}
+	
+	public BTree getChild(int index){
+		return listChildren.get(index);
+	}
+	
+	public List<Integer> getKeys(BTree tree){
+		return tree.listKey;
+	}
+	
+	public void addKey(int value){
+		listKey.add(value);
+	}
+	
+	public void addChild(BTree child){
+		this.listChildren.add(child);
+	}
+	
+	public void removeKey(int index){
+		listKey.remove(index);
+	}
+	
+	public void removeChild(int index){
+		listChildren.remove(index);
+	}
+	
+	public boolean isLeaf(BTree tree){
+		if(tree.listChildren.isEmpty()){
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean canAddKey(BTree tree){
+		if(tree.listKey.size()<n){
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean canAddChild(BTree tree){
+		if(tree.listChildren.size()<n+1){
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean isInKey(int value, BTree tree){
+		if(tree.listKey.contains(value)){
+			return true;
+		}
+		return false;
+	}
+	
+	public void toString(BTree tree){
+		String res="Valeurs noeud: ";
+		int i =0;
+		while(i < tree.getKeys(tree).size()){
+			res = res + tree.getKey(i);
+			i++;
+		}
+		System.out.println(res);
+	}
+	
+	public boolean search(int key, BTree tree){
+		//boolean res=false;
+		if(tree == null){
+			res=false;
+		}else if(key < tree.getKey(0)){
+			System.out.println("Parcours du fils 0");
+			search(key, tree.getChild(0));
+		}else if(key> tree.getKey(tree.listKey.size()-1)){
+			System.out.println("Parcours du fils n");
+			search(key, tree.getChild(tree.listKey.size()));
+		}else if(searchInNode(key,tree)==true){
+			System.out.println("Clause search vraie");
+			res=true;
+		}else{
+			System.out.println(treeToSearchIn);
+			search(key,treeToSearchIn);
+		}
+		System.out.println("res="+res);
+		return res;
+	}
+	
+	public boolean searchInNode(int key, BTree tree){
+		System.out.println("Recherche dans noeud");
+		int g = 0;
+		int d = tree.listKey.size();
+		System.out.println("d init="+d);
+		int m = 0;
+		
+		while(g!=d){
+			m = (g+d)/2;
+			System.out.println("m="+m);
+			if(tree.getKey(m) >= key){
+				d = m;
+				System.out.println("d="+d);
+			}else{
+				g = m+1;
+				System.out.println("g="+g);
+			}
+		}
+		
+		if(tree.getKey(g)==key){
+			System.out.println("Clause dans noeud vrai");
+			treeToSearchIn = null;
+			return true;
+		}else{
+			System.out.println("Abre a parcourir");
+			treeToSearchIn = tree.getChild(g);
+			System.out.println(treeToSearchIn);
+		}
+		return false;
+	}
+
+}
---
+++ ProjetAlgo/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args){
+		
+		List<Integer> l1 = new ArrayList<Integer>();
+		l1.add(2);
+		l1.add(4);
+		BTree root = new BTree(l1,new ArrayList<BTree>());
+		
+		List<Integer> l2 = new ArrayList<Integer>();
+		l2.add(1);
+		BTree tree1 = new BTree(l2,new ArrayList<BTree>());
+		root.addChild(tree1);
+		
+		List<Integer> l3 = new ArrayList<Integer>();
+		l3.add(3);
+		BTree tree2 = new BTree(l3, new ArrayList<BTree>());
+		root.addChild(tree2);
+		
+		List<Integer> l4 = new ArrayList<Integer>();
+		l4.add(5);
+		l4.add(6);
+		BTree tree3 = new BTree(l4,new ArrayList<BTree>());
+		root.addChild(tree3);
+		
+		root.toString(root);
+		System.out.println(1/2);
+		System.out.println(root.search(3,root));
+		
+	}
+
+}
